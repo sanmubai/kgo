8edc61be17dd5f58b33cb7109362a5cef8f62093
baibin <sanmubai@foxmail.com>
2021-10-14 12:05:54 +0800

    cliRender,PrettyFileSize

--- string.go
+++ string.go
@@ -2208,3 +2208,21 @@ func (ks *LkkString) GetEquationValue(str, name string) (res string) {
 
 	return
 }
+
+// PrettyFileSize 把bit转化为更大的单位显示
+func (ks *LkkString) PrettyFileSize(fileSize int64) (size string) {
+	if fileSize < 1024 {
+		//return strconv.FormatInt(fileSize, 10) + "B"
+		return fmt.Sprintf("%.2fB ", float64(fileSize)/float64(1))
+	} else if fileSize < (1024 * 1024) {
+		return fmt.Sprintf("%.2fKB ", float64(fileSize)/float64(1024))
+	} else if fileSize < (1024 * 1024 * 1024) {
+		return fmt.Sprintf("%.2fMB ", float64(fileSize)/float64(1024*1024))
+	} else if fileSize < (1024 * 1024 * 1024 * 1024) {
+		return fmt.Sprintf("%.2fGB ", float64(fileSize)/float64(1024*1024*1024))
+	} else if fileSize < (1024 * 1024 * 1024 * 1024 * 1024) {
+		return fmt.Sprintf("%.2fTB ", float64(fileSize)/float64(1024*1024*1024*1024))
+	} else { //if fileSize < (1024 * 1024 * 1024 * 1024 * 1024 * 1024)
+		return fmt.Sprintf("%.2fEB ", float64(fileSize)/float64(1024*1024*1024*1024*1024))
+	}
+}
