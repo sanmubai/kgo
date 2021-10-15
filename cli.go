0dde183a2cedb631a95dbd856c876e386ad132e4
baibin <sanmubai@foxmail.com>
2021-10-15 12:10:26 +0800

    cli

--- cli.go
+++ cli.go
@@ -91,7 +91,7 @@ func (kc *LKKCli) FormatStrWithColor(str string,args ...interface{}) string{
         }
         args[i]=kc.ColorWrapStr(s,color)
     }
-    return fmt.Sprintf(str,args[0:len(args)-1])
+    return fmt.Sprintf(str,args[0:len(args)-1]...)
 }
 
 // Green 绿色字体，modifier里，第一个控制闪烁，第二个控制下划线
